cbe313956a2a61e81ba1b130ad04fbb486133587
Reclocer <54275125+Reclocer@users.noreply.github.com>
2020-03-30 16:26:25 +0300

    Fixed localization

--- Assets/Scripts/Localization/Localization.cs
+++ Assets/Scripts/Localization/Localization.cs
@@ -10,40 +10,15 @@ public class Localization : MonoBehaviour
 {
     private string _language = "Ru";
     public  string  Language => _language;
+    
+    public event Action<string> OnChangeLanguage = (language) => { };    
 
-    private List<string> _names = new List<string>();
-    private GameObject[] _words;
-    public event Action<string> OnChangeLanguage = (language) => { };
-
-    private void Start()
-    {
-        LoadLanguage();        
-    }
-
+    /// <summary>
+    /// Change language
+    /// </summary>
     public void ClickLanguageChangeBtn()
     {
-//#if UNITY_EDITOR && !UNITY_ANDROID
-//        if (_language == "En")
-//        {
-//            _language = "Ru";
-//            PlayerPrefs.SetString("Language", _language);
-//            PlayerPrefs.Save();
 
-//            LoadLanguage();            
-//            SceneManager.LoadScene(0);
-//        }
-//        else
-//        {
-//            _language = "En";
-//            PlayerPrefs.SetString("Language", _language);
-//            PlayerPrefs.Save();
-
-//            LoadLanguage();
-//            SceneManager.LoadScene(0);
-//        }  
-//#endif
-
-//#if UNITY_EDITOR && UNITY_ANDROID
         if (_language == "En")
         {
             _language = "Ru";
@@ -56,34 +31,5 @@ public class Localization : MonoBehaviour
         }
 
         OnChangeLanguage(_language);
-//#endif
-    }
-
-    private void LoadLanguage()
-    {
-//#if UNITY_EDITOR && !UNITY_ANDROID
-//        if (PlayerPrefs.HasKey("Language"))
-//        {
-//            _words = GameObject.FindGameObjectsWithTag("Localizable");
-//            string language = PlayerPrefs.GetString("Language");
-//            XmlDocument xmlDocument = new XmlDocument();
-//            xmlDocument.Load($"Assets/Resources/Localization/{language}.xml");
-//            XmlNodeList list = xmlDocument.GetElementsByTagName("word");
-//            int lenght = _words.Length;
-
-//            foreach (XmlNode tag in list)
-//            {
-//                _names.Add(tag.InnerText);
-//            }
-
-//            for (int i = 0; i < lenght; i++)
-//            {
-//                _words[i].GetComponent<Text>().text = _names[i];
-//            }
-            
-//            _language = language;
-//            GetComponentInChildren<Text>().text = language;
-//        }
-//#endif
-    }
+    }    
 }
